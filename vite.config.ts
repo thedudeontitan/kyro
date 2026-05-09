b6e8eb6abb2da84ae3e5ea005c4b652780ef4e8a
Ankit Kokane <ankitkokane90@gmail.com>
2026-05-09 14:15:00 +0530

    chore: add Solana dependencies and update build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), nodePolyfills({ include: ["buffer"] })],
+  define: {
+    global: "globalThis",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
